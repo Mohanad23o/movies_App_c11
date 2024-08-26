c64a1e36e2a6d959626bcc1b9b32079e7164d387
mohanad <Mohanad.Github@gmail.com>
2024-08-26 19:24:23 +0300

    Fixed Error

--- lib/repository/genries_data_source.dart
+++
@@ -1,5 +0,0 @@
-import 'package:movies_app_c11/model/Categories_Response.dart';
-
-abstract class GenriesRemoteDataSource {
-  Future<CategoriesResponse?> getGenries();
-}
--- lib/tabs/category_browse/category_browse/cubit/category_browse_tab_view_model.dart
+++ lib/tabs/category_browse/category_browse/cubit/category_browse_tab_view_model.dart
@@ -1,30 +1,21 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app_c11/api/api_manager.dart';
 import 'package:movies_app_c11/model/Categories_Response.dart';
-import 'package:movies_app_c11/repository/genries/dataSources/genries_remote_data_source_impl.dart';
-import 'package:movies_app_c11/repository/genries/repository/genries_repository_impl.dart';
-import 'package:movies_app_c11/repository/genries_data_source.dart';
-import 'package:movies_app_c11/repository/genries_repository_contract.dart';
 
 import 'Sources_State.dart';
 
 class CategoryBrowseTabViewModel extends Cubit<CategorySourcesState> {
-  late GenriesRepositoryContract repositoryContract;
-  late GenriesRemoteDataSource remoteDataSource;
   late ApiManager apiManager;
 
   CategoryBrowseTabViewModel() : super(SourceLoadingState()) {
     apiManager = ApiManager();
-    remoteDataSource = GenriesRemoteDataSourceImpl(apiManager: apiManager);
-    repositoryContract =
-        GenriesRepositoryImpl(remoteDataSource: remoteDataSource);
   }
 
   List<Genres>? category;
 
   Future<void> getGenries() async {
     emit(GenriesLoadingState());
-    var response = await repositoryContract.getGenries();
+    var response = await apiManager.getGenries();
 
     try {
       if (response?.success != false) {

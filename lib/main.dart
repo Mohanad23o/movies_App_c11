3ae4c445f1ea437bf1fbc99aca914ef2c526bb15
mohanad <Mohanad.Github@gmail.com>
2024-08-26 21:03:53 +0300

    Added FireBase To The Project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app_c11/my_bloc_observer.dart';
@@ -7,9 +10,25 @@ import 'package:movies_app_c11/screens/splash/custom_splash_screen.dart';
 import 'package:movies_app_c11/tabs/category_browse/category_browse/category_details/CategoryDetails.dart';
 import 'package:movies_app_c11/theme/my_theme_data.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
-  runApp(const MyApp());
+  try {
+    if (Platform.isAndroid) {
+      await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: 'AIzaSyD47oYbl--p7qpKn4nfQ3qrp-mCKycwp6Q',
+              appId: 'com.example.movies_app_c11',
+              messagingSenderId: '110654541466',
+              projectId: 'movies-app-c11'));
+    } else {
+      await Firebase.initializeApp();
+    }
+    // await FirebaseFirestore.instance.enableNetwork();
+    runApp(const MyApp());
+  } catch (e) {
+    print(e);
+  }
 }
 
 class MyApp extends StatelessWidget {
